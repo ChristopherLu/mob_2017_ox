24330875ebda244e1d6cfb60f76f3a825e1df99a
Matthias Görgens <matthias.goergens@gmail.com>
2017-06-20 10:41:54 +0100

    Clean up (#1)
    * Don't include *.pyc
    
    * Ignore .pyc
    
    * Fix tabs vs space
    
    The files had a mix between tabs and spaces.  I removed the tabs.
    
    * Remove all trailing whitespace
    
    * Add spaces after comma
    
    * Fix indentation to make pylint happier

--- lab_1/p1_MAC_codebase/p1_csma.py
+++ lab_1/p1_MAC_codebase/p1_csma.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as p
 ###############################################################
 
 class CSMANode(WirelessNode):
-    def __init__(self,location,network,retry):
-        WirelessNode.__init__(self,location,network,retry)
+    def __init__(self, location, network, retry):
+        WirelessNode.__init__(self, location, network, retry)
 
         # for plots of collisions/success
         self.sent = []
@@ -18,20 +18,20 @@ class CSMANode(WirelessNode):
         # initialize local probability of transmission
         self.p = self.network.pmax
 
-    def channel_access(self,time,ptime,numnodes):
+    def channel_access(self, time, ptime, numnodes):
         ## TODO: decide whehter to transmit
         ## You can tell if the channel is busy or not using
         ## the self.network.channel_busy() function call.
         pass
 
-    def on_collision(self,packet):
+    def on_collision(self, packet):
         # for plots of collisions
         self.coll.append(self.network.time)
 
         ## TODO:Decrease the probability
-        pass 
+        pass
 
-    def on_xmit_success(self,packet):
+    def on_xmit_success(self, packet):
         # for plots of successful transmissions
         self.sent.append(self.network.time)
 
@@ -41,47 +41,47 @@ class CSMANode(WirelessNode):
 ################################################################
 
 class CSMAWirelessNetwork(WirelessNetwork):
-    def __init__(self,n,chantype,ptime,dist,load,retry,backoff,
-		 skew,qmax,pmax,pmin,simtime):
+    def __init__(self, n, chantype, ptime, dist, load, retry, backoff,
+                 skew, qmax, pmax, pmin, simtime):
         self.pmax = pmax
-        self.pmin = pmin        
-        WirelessNetwork.__init__(self,n,chantype,ptime,dist,load,retry,backoff,
-                                 skew,qmax,simtime)
+        self.pmin = pmin
+        WirelessNetwork.__init__(self, n, chantype, ptime, dist, load, retry, backoff,
+                                 skew, qmax, simtime)
 
-    def make_node(self,loc,retry):
-        return CSMANode(loc,self,retry)
+    def make_node(self, loc, retry):
+        return CSMANode(loc, self, retry)
 
 ################################################################
 
 if __name__ == '__main__':
     random.seed(6172538) # For repeatability
     parser = OptionParser()
-    parser.add_option("-g", "--gui", action="store_true", dest="gui", 
+    parser.add_option("-g", "--gui", action="store_true", dest="gui",
                       default=False, help="show GUI")
-    parser.add_option("-n", "--numnodes", type="int", dest="numnodes", 
+    parser.add_option("-n", "--numnodes", type="int", dest="numnodes",
                       default=16, help="number of nodes")
-    parser.add_option("-t", "--simtime", type="int", dest="simtime", 
+    parser.add_option("-t", "--simtime", type="int", dest="simtime",
                       default=10000, help="simulation time")
-    parser.add_option("-b", "--backoff", dest="backoff", 
+    parser.add_option("-b", "--backoff", dest="backoff",
                       default='Mine', help="backoff scheme (Mine, None)")
-    parser.add_option("-s", "--size", type="int", dest="ptime", 
+    parser.add_option("-s", "--size", type="int", dest="ptime",
                       default=1, help="packet size (in time units)")
-    parser.add_option("-p", "--pmax", type="float", dest="pmax", 
-                      default=1.0, help="max probability of xmission")    
-    parser.add_option("-q", "--pmin", type="float", dest="pmin", 
-                      default=0.0, help="min probability of xmission")    
-    parser.add_option("-l", "--load", type="int", dest="load", 
+    parser.add_option("-p", "--pmax", type="float", dest="pmax",
+                      default=1.0, help="max probability of xmission")
+    parser.add_option("-q", "--pmin", type="float", dest="pmin",
+                      default=0.0, help="min probability of xmission")
+    parser.add_option("-l", "--load", type="int", dest="load",
                       default=100, help="total load % (in pkts/timeslot)")
-    parser.add_option("-r", "--retry", action="store_true", dest="retry", 
+    parser.add_option("-r", "--retry", action="store_true", dest="retry",
                       default=False, help="show GUI")
-    parser.add_option("-k", "--skew", action="store_true", dest="skew", 
+    parser.add_option("-k", "--skew", action="store_true", dest="skew",
                       default=False, help="skew source loads")
 
     (opt, args) = parser.parse_args()
     print 'Protocol: CSMA with stabilization'
-    wnet = CSMAWirelessNetwork(opt.numnodes,'CSMA',opt.ptime,
-                               'exponential',opt.load,opt.retry,opt.backoff,
-                               opt.skew,0,opt.pmax,opt.pmin,opt.simtime)
+    wnet = CSMAWirelessNetwork(opt.numnodes, 'CSMA', opt.ptime,
+                               'exponential', opt.load, opt.retry, opt.backoff,
+                               opt.skew, 0, opt.pmax, opt.pmin, opt.simtime)
 
     if opt.gui == True:
         sim = NetSim()
--- lab_1/p1_MAC_codebase/p1_stabaloha.py
+++ lab_1/p1_MAC_codebase/p1_stabaloha.py
@@ -8,8 +8,8 @@ import matplotlib.pyplot as p
 ###############################################################
 
 class AlohaNode(WirelessNode):
-    def __init__(self,location,network,retry):
-        WirelessNode.__init__(self,location,network,retry)
+    def __init__(self, location, network, retry):
+        WirelessNode.__init__(self, location, network, retry)
 
         # for plots of collisions/success
         self.sent = []
@@ -18,18 +18,18 @@ class AlohaNode(WirelessNode):
         # initialize local probability of transmission
         self.p = self.network.pmax
 
-    def channel_access(self,time,ptime,numnodes):
-        ## TODO: control channel access 
+    def channel_access(self, time, ptime, numnodes):
+        ## TODO: control channel access
         pass
 
-    def on_collision(self,packet):
+    def on_collision(self, packet):
         # for plots of collisions
         self.coll.append(self.network.time)
 
         ## TODO: Decrease the probability
-        pass 
+        pass
 
-    def on_xmit_success(self,packet):
+    def on_xmit_success(self, packet):
         # for plots of success
         self.sent.append(self.network.time)
 
@@ -39,48 +39,48 @@ class AlohaNode(WirelessNode):
 ################################################################
 
 class AlohaWirelessNetwork(WirelessNetwork):
-    def __init__(self,n,chantype,ptime,dist,load,retry,backoff,
-		 skew,qmax,pmax,pmin,simtime):
+    def __init__(self, n, chantype, ptime, dist, load, retry, backoff,
+                 skew, qmax, pmax, pmin, simtime):
         self.pmax = pmax
         self.pmin = pmin
-        WirelessNetwork.__init__(self,n,chantype,ptime,dist,load,retry,backoff,
-                                 skew,qmax,simtime)
+        WirelessNetwork.__init__(self, n, chantype, ptime, dist, load, retry, backoff,
+                                 skew, qmax, simtime)
 
-    def make_node(self,loc,retry):
-        return AlohaNode(loc,self,retry)
+    def make_node(self, loc, retry):
+        return AlohaNode(loc, self, retry)
 
 ################################################################
 
 if __name__ == '__main__':
     random.seed(6172538) # For repeatability
     parser = OptionParser()
-    parser.add_option("-g", "--gui", action="store_true", dest="gui", 
+    parser.add_option("-g", "--gui", action="store_true", dest="gui",
                       default=False, help="show GUI")
-    parser.add_option("-n", "--numnodes", type="int", dest="numnodes", 
+    parser.add_option("-n", "--numnodes", type="int", dest="numnodes",
                       default=16, help="number of nodes")
-    parser.add_option("-t", "--simtime", type="int", dest="simtime", 
+    parser.add_option("-t", "--simtime", type="int", dest="simtime",
                       default=10000, help="simulation time")
-    parser.add_option("-b", "--backoff", dest="backoff", 
+    parser.add_option("-b", "--backoff", dest="backoff",
                       default='Mine', help="backoff scheme (Mine, None)")
-    parser.add_option("-s", "--size", type="int", dest="ptime", 
+    parser.add_option("-s", "--size", type="int", dest="ptime",
                       default=1, help="packet size (in time units)")
-    parser.add_option("-p", "--pmax", type="float", dest="pmax", 
-                      default=1.0, help="max probability of xmission")    
-    parser.add_option("-q", "--pmin", type="float", dest="pmin", 
-                      default=0.0, help="min probability of xmission")    
-    parser.add_option("-l", "--load", type="int", dest="load", 
+    parser.add_option("-p", "--pmax", type="float", dest="pmax",
+                      default=1.0, help="max probability of xmission")
+    parser.add_option("-q", "--pmin", type="float", dest="pmin",
+                      default=0.0, help="min probability of xmission")
+    parser.add_option("-l", "--load", type="int", dest="load",
                       default=100, help="total load % (in pkts/timeslot)")
-    parser.add_option("-r", "--retry", action="store_true", dest="retry", 
+    parser.add_option("-r", "--retry", action="store_true", dest="retry",
                       default=False, help="show GUI")
-    parser.add_option("-k", "--skew", action="store_true", dest="skew", 
+    parser.add_option("-k", "--skew", action="store_true", dest="skew",
                       default=False, help="skew source loads")
 
     (opt, args) = parser.parse_args()
     print 'Protocol: Aloha with stabilization'
-    wnet = AlohaWirelessNetwork(opt.numnodes,'Aloha',opt.ptime,
-                                'exponential',opt.load,opt.retry,opt.backoff,
-                                opt.skew,0,opt.pmax,opt.pmin,opt.simtime)
-    
+    wnet = AlohaWirelessNetwork(opt.numnodes, 'Aloha', opt.ptime,
+                                'exponential', opt.load, opt.retry, opt.backoff,
+                                opt.skew, 0, opt.pmax, opt.pmin, opt.simtime)
+
     if opt.gui == True:
         sim = NetSim()
         sim.SetNetwork(wnet)
